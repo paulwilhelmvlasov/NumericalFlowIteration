cb166ab500113e15755a0914f4daa4feec91280f
Paul Wilhelm <wilhelm@acom.rwth-aachen.de>
2022-03-10 18:29:56 +0100

    finially a working poisson 1d solver. tomorrow will write 2d and 3d versions

--- main.cpp
+++ main.cpp
@@ -1,82 +1,91 @@
 #include <iostream>
 #include <fstream>
-//#include "periodic_poisson_solver.h"
 #include <math.h>
 #include <fftw3.h>
 
+double rho(double x)
+{
+	return std::sin(x);
+}
 
 int main(int argc, char **argv) {	
-	size_t N = 21;
-	size_t N_mid = N/2 + 1;
-	double L = 2 * M_PI;
-	double dx = L / double(N - 1);
+	double Pi = M_PI;
+	    
+	/// Grid size
+	int Nx = 2001;
+	int Nxh = (Nx/2+1);
 
-	fftw_complex *rho, *rho_transformed;
-	fftw_complex *phi_transformed, *phi;
-	fftw_plan p;
-	
-	// Initialize data:
-	rho = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-	rho_transformed = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-	phi_transformed = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-	phi = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+	/// Declare FFTW components.
+	fftw_complex *mem;
+	fftw_complex *out;
+	double *in;
 
-	for(size_t i = 0; i < N; i++)
+	out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * Nx );
+	in = new double[Nx];
+
+	// Init in:
+	double L0=0.0;
+	double L1=2*Pi;
+	double xlen = (L1-L0);
+	double dx = xlen / Nx;
+
+	for(size_t i = 0; i < Nx; i++)
 	{
-		rho[i][0] = std::sin(i * dx);
-		rho[i][1] = 0;		
+		in[i] = rho(i * dx);
 	}
 
-	// FFT of rho:
-	p = fftw_plan_dft_1d(N, rho, rho_transformed, FFTW_FORWARD, FFTW_ESTIMATE);
-	fftw_execute(p);
+	for(size_t i = 0; i < Nx; i++)
+		std::cout << in[i] << std::endl;
 
-	// Compute coefficients for transformed phi:
-	phi_transformed[0][0] = 0;
-	phi_transformed[0][1] = 0;
-//	for(size_t i = 0; i < N_mid; i++)
-	for(size_t i = 1; i < N; i++)
-	{
-//		size_t k = N_mid - i; 
-		size_t k = i; 
-		double k_sq = std::pow(2 * M_PI * k / L, 2) * std::sqrt(N);
-		phi_transformed[i][0] = -rho_transformed[i][0] / k_sq;
-		phi_transformed[i][1] = -rho_transformed[i][1] / k_sq;
-	}
-/*	phi_transformed[N_mid][0] = 0;
-	phi_transformed[N_mid][1] = 0;
-	for(size_t i = N_mid + 1; i < N; i++)
-	{
-		size_t k = i - N_mid; 
-		double k_sq = std::pow(2 * M_PI * k / L, 2);
-		phi_transformed[i][0] = -rho_transformed[i][0] / k_sq;
-		phi_transformed[i][1] = -rho_transformed[i][1] / k_sq;
-	}
-*/
-	// FFT backwards for phi:
-	p = fftw_plan_dft_1d(N, phi_transformed, phi, FFTW_BACKWARD, FFTW_ESTIMATE);
-	fftw_execute(p);
 
-	for(size_t i = 0; i < N; i++)
-	{
-		phi[i][0] /= std::sqrt(N);
-		phi[i][1] /= std::sqrt(N);
-	}
+	fftw_plan fwrd = fftw_plan_dft_r2c_1d(Nx,in,out,FFTW_ESTIMATE);
+	fftw_plan bwrd = fftw_plan_dft_c2r_1d(Nx,out,in,FFTW_ESTIMATE);
+		
+	fftw_execute(fwrd);
 
-	std::ofstream str_rho("rho.txt");
-	std::ofstream str_rho_tr("rho_tr.txt");
+	for(size_t i = 0; i < Nx; i++)
+		std::cout << out[i][0] << " " << out[i][1] << std::endl;
+
+	int II = -5;
+	double k1;
+	for (int i=0;i<Nx;i++)
+	{
+		if (2*i<Nx)
+			II = i;
+		else
+			II = Nx-i;
+		
+		k1 = 2*Pi*II/xlen;
+				
+		double fac = -1.0*pow(k1,2)*Nx;
+		if (fabs(fac) < 1e-14)
+		{
+			out[i][0] = 0.0;
+			out[i][1] = 0.0;
+		}
+		else
+		{
+			out[i][0] /= fac;
+			out[i][1] /= fac;
+		}
+		std::cout << out[i][0] << " " << out[i][1] << std::endl;
+	}
+        
+	fftw_execute(bwrd);
+
+	for(size_t i = 0; i < Nx; i++)
+		std::cout << in[i] << std::endl;
+        
 	std::ofstream str_phi("phi.txt");
-	std::ofstream str_phi_tr("phi_tr.txt");
-	
-	for(size_t i = 0; i < N; i++)
+
+	for(size_t i = 0; i < Nx; i++ )
 	{
 		double x = i * dx;
-		str_rho << x << " " << rho[i][0] << " " << rho[i][1] << std::endl;
-		str_rho_tr << x << " " << rho_transformed[i][0] << " " << rho_transformed[i][1] << std::endl;
-		str_phi_tr << x << " " << phi_transformed[i][0] << " " << phi_transformed[i][1] << std::endl;
-		str_phi << x << " " << phi[i][0] << " " << phi[i][1] << std::endl;
+		double phi = in[i];
+		str_phi << x << " " << phi << std::endl;
 	}
-	
+
+	delete [] in;
 
 	return 0;
 }
