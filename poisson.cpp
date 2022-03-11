39312485351cfd157290f619ef046e820b0dbf4d
Paul Wilhelm <wilhelm@acom.rwth-aachen.de>
2022-03-11 17:37:37 +0100

    added 2d poisson solver classes in same way as 1d. Care: Not tested or compiled yet!!!

--- poisson.cpp
+++ poisson.cpp
@@ -5,6 +5,7 @@ namespace dergeraet
 
 namespace dim1
 {
+/************************************************************************************************/
 	// Definition for real = double.
 	template <>
 	poisson<double>::poisson( const config_t<double> &param )
@@ -23,7 +24,7 @@ namespace dim1
 	}
 
 	template <>
-	poisson<double>::~poisson() { }
+	poisson<double>::~poisson() { fftw_free(out); }
 
 	template <>
 	void poisson<double>::solve( double *rho, double *phi )
@@ -72,6 +73,7 @@ namespace dim1
 		fftw_free(in);
 	}
 
+/************************************************************************************************/
 	// Definition for real = float.
 	template <>
 	poisson<float>::poisson( const config_t<float> &param )
@@ -90,7 +92,7 @@ namespace dim1
 	}
 
 	template <>
-	poisson<float>::~poisson() { }
+	poisson<float>::~poisson() { fftwf_free(out); }
 
 	template <>
 	void poisson<float>::solve( float *rho, float *phi )
@@ -98,7 +100,7 @@ namespace dim1
 		fftwf_execute(forward, rho, out);
 
 		int II = -5;
-		double k1;
+		float k1;
 		for (int i = 0; i < param.Nx; i++)
 		{
 			if (2*i<param.Nx)
@@ -108,7 +110,7 @@ namespace dim1
 			
 			k1 = 2*Pi*II/param.Lx;
 					
-			double fac = -1.0*k1*k1*param.Nx;
+			float fac = -1.0*k1*k1*param.Nx;
 			if (fabs(fac) < 1e-14)
 			{
 				out[i][0] = 0.0;
@@ -140,6 +142,158 @@ namespace dim1
 		fftwf_free(in);
 	}
 }
+/************************************************************************************************/
+
+namespace dim2
+{
+/************************************************************************************************/
+	// Definition for real = double.
+	template <>
+	poisson<double>::poisson( const config_t<double> &param )
+		: param { param }
+	{
+		// Note: We do not reuse the out array but instead pass arrays everytime the 
+		// solve method is called. These arrays have to have the same shape as the initial
+		// in array.
+		out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * param.Nx * param.Ny );
+		double *in = (double *) fftw_malloc(sizeof(double) * param.Nx * param.Ny );
+
+		forward = fftw_plan_dft_r2c_2d(param.Nx,param.Ny,in,out,FFTW_MEASURE);
+		backward = fftw_plan_dft_c2r_2d(param.Nx,param.Ny,out,in,FFTW_MEASURE);
+
+		fftw_free(in);
+	}
+
+	template <>
+	poisson<double>::~poisson() { fftw_free(out); }
+
+	template <>
+	void poisson<double>::solve( double *rho, double *phi )
+	{
+		fftw_execute(forward, rho, out);
+
+		int II,JJ;
+		double k1,k2;
+		for (int i=0;i<param.Nx;i++)
+		{
+			if (2*i<param.Nx)
+				II = i;
+			else
+				II = param.Nx-i;
+			k1 = 2*Pi*II/param.Lx;
+		        
+			for (int j=0;j<param.Ny;j++)
+			{
+		    		if (2*j<param.Ny)
+		        		JJ = j;
+		    		else
+		        		JJ = param.Ny-j;
+		    		k2 = 2*Pi*JJ/param.Ly;
+		                
+			        double fac = -1.0*(k1*k1 + k2*k2)*param.Nx*param.Ny;
+				size_t curr = j + param.Ny * i;
+			        if (fabs(fac) < 1e-14)
+		        	{
+		            		out[curr][0] = 0.0;
+		            		out[curr][1] = 0.0;
+		        	}
+		        	else
+		        	{
+		            		out[curr][0] /= fac;
+		            		out[curr][1] /= fac;
+		        	}                
+			}
+		}
+
+		fftw_execute(backward, out, phi);
+	}
+
+	template <>
+	void poisson<double>::conf( const config_t<double> &new_param )
+	{
+		// Note: This method not only updates the parameters but also has to
+		// recompute the plans. This can take some time.
+		param = new_param;
+
+		double *in = (double *) fftw_malloc(sizeof(double) * param.Nx * param.Ny);
+
+		forward = fftw_plan_dft_r2c_2d(param.Nx,param.Ny,in,out,FFTW_MEASURE);
+		backward = fftw_plan_dft_c2r_2d(param.Nx,param.Ny,out,in,FFTW_MEASURE);
+
+		fftw_free(in);
+	}
+
+/************************************************************************************************/
+	// Definition for real = float.
+	template <>
+	poisson<float>::poisson( const config_t<float> &param )
+		: param { param }
+	{
+		// Note: We do not reuse the out array but instead pass arrays everytime the 
+		// solve method is called. These arrays have to have the same shape as the initial
+		// in array.
+		out = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * param.Nx * param.Ny );
+		float *in = (float *) fftwf_malloc(sizeof(float) * param.Nx * param.Ny );
+
+		forward = fftwf_plan_dft_r2c_2d(param.Nx,param.Ny,in,out,FFTW_MEASURE);
+		backward = fftwf_plan_dft_c2r_2d(param.Nx,param.Ny,out,in,FFTW_MEASURE);
+
+		fftwf_free(in);
+	}
+
+	template <>
+	poisson<float>::~poisson() { fftwf_free(out); }
+
+	template <>
+	void poisson<float>::solve( float *rho, float *phi )
+	{
+		fftwf_execute(forward, rho, out);
+
+		int II = -5;
+		float k1;
+		for (int i = 0; i < param.Nx; i++)
+		{
+			if (2*i<param.Nx)
+				II = i;
+			else
+				II = param.Nx-i;
+			
+			k1 = 2*Pi*II/param.Lx;
+					
+			float fac = -1.0*k1*k1*param.Nx;
+			if (fabs(fac) < 1e-14)
+			{
+				out[i][0] = 0.0;
+				out[i][1] = 0.0;
+			}
+			else
+			{
+				out[i][0] /= fac;
+				out[i][1] /= fac;
+			}
+		}
+
+		fftwf_execute(backward, out, phi);
+	}
+
+	template <>
+	void poisson<float>::conf( const config_t<float> &new_param )
+	{
+		// Note: This method not only updates the parameters but also has to
+		// recompute the plans. This can take some time.
+		param = new_param;
+
+		float *in = (float *) fftwf_malloc(sizeof(float) * param.Nx * param.Ny);
+		
+		// Note: Upper-case FFTW remains the same even for float (or long double).
+		forward = fftwf_plan_dft_r2c_2d(param.Nx,param.Ny,in,out,FFTW_MEASURE); 
+		backward = fftwf_plan_dft_c2r_2d(param.Nx,param.Ny,out,in,FFTW_MEASURE);
+
+		fftwf_free(in);
+	}
+}
+
+
 
 }
 
--- poisson.hpp
+++ poisson.hpp
@@ -62,3 +62,64 @@ namespace dim1
 
 }
 
+namespace dim2
+{
+
+	template <typename real> class poisson;
+
+	// Defintion for real = double.
+	template <>
+	class poisson<double>
+	{
+	public:
+		poisson() = delete;
+		poisson( config_t<double> &param );
+
+		~poisson();
+
+
+		config_t conf() { return conf_; }
+		void     conf( const config_t<double> &new_param );
+
+		void solve( double *rho, double *phi );
+
+	private:
+		config_t<double> param;
+
+		fftw_complex *out;
+
+		fftw_plan forward;
+		fftw_plan backward;
+	};
+
+	// Declaration for real = float.
+	template <>
+	class poisson<float>
+	{
+	public:
+		poisson() = delete;
+		poisson( config_t<float> &param );
+
+		~poisson();
+
+
+		config_t conf() { return conf_; }
+		void     conf( const config_t<float> &new_param );
+
+		void solve( float *rho, double *phi );
+
+	private:
+		config_t<float> param;
+
+		fftwf_complex *out;
+
+		fftwf_plan forward;
+		fftwf_plan backward;
+	};
+
+
+}
+
+
+}
+
