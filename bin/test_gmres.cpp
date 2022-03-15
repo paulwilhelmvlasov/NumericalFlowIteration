25d2e0a4b2f07e1a46fc0ec2835f257d0b18e568
Matthias Kirchhart <kirchhart@acom.rwth-aachen.de>
2022-03-15 18:04:15 +0100

    Added GMRES solver.

---
+++ bin/test_gmres.cpp
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2022 Matthias Kirchhart and Paul Wilhelm
+ *
+ * This file is part of Der Gerät, a solver for the Vlasov–Poisson equation.
+ *
+ * Der Gerät is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3, or (at your option) any later
+ * version.
+ *
+ * Der Gerät is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Der Gerät; see the file COPYING.  If not see http://www.gnu.org/licenses.
+ */
+
+#include <memory>
+#include <iomanip>
+#include <iostream>
+#include <dergeraet/gmres.hpp>
+#include <dergeraet/random.hpp>
+
+int main()
+{
+    constexpr size_t N = 100;
+    std::unique_ptr<double[]> mem { new double[ N*(N+3) ] };
+
+    dergeraet::random_real<double> rand(-1,1);
+
+    double *A = mem.get();
+    double *b = A + N*N;
+    double *x = b + N;
+    double *test = x + N;
+
+    for ( size_t i = 0; i < N*N; ++i )
+        A[ i ] = rand();
+
+    for ( size_t i = 0; i < N; ++i )
+    {
+        x[ i ] = 0;
+        b[ i ] = rand();
+        A[ i + i*N ] += 10;
+    }
+
+    struct matmul_t
+    {
+        double *A;
+
+        void operator()( const double *x, size_t stride_x,
+                               double *v, size_t stride_y ) const noexcept
+        {
+            for ( size_t i = 0; i < N; ++i )
+            {
+                double val = 0;
+                for ( size_t j = 0; j < N; ++j )
+                {
+                    val += A[ i + j*N ]*x[ j*stride_x ];
+                }
+                v[ i*stride_y ] = val;
+            }
+        }
+    };
+
+    matmul_t mat { A };
+    dergeraet::gmres_config<double> opt; opt.target_residual = 1e-9;
+    dergeraet::gmres<double,matmul_t>( N, x, 1, b, 1, mat, opt );
+
+    mat( x, 1, test, 1 );
+
+    double resid = 0, norm_b = 0;
+    for ( size_t i = 0; i < N; ++i )
+    {
+        resid  = hypot( resid, b[i]-test[i] );
+        norm_b = hypot( norm_b, b[i] );
+    }
+
+    std::cout << "Actual residuals: absolute: " << std::setw(15) << resid << ' '
+              << "relative: " << std::setw(15) << resid/norm_b << std::endl;
+}
+
