5e07059c03377cfb03c981b2005cb840646e57b8
Paul Wilhelm <wilhelm@acom.rwth-aachen.de>
2022-10-21 15:39:10 +0200

    testing

--- dergeraet/config.hpp
+++ dergeraet/config.hpp
@@ -181,8 +181,8 @@ struct config_t
 template <typename real>
 config_t<real>::config_t() noexcept
 {
-    Nx = Ny = 32;
-    Nu = Nv = 64;
+    Nx = Ny = 64;
+    Nu = Nv = 128;
     u_min = v_min = -6;
     u_max = v_max =  6;
     x_min = y_min = 0;
