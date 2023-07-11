fbbefb121259d7d0d04c72b26e8a214bbdd55da5
Paul Wilhelm <wilhelm@acom.rwth-aachen.de>
2023-07-11 16:13:43 +0200

    we need ** pointers to communicate with fortran

--- bin/test_cpp_interface.cpp
+++ bin/test_cpp_interface.cpp
@@ -7,52 +7,75 @@
 //#include <ostream>
 #include <iostream>
 #include <vector>
+#include <cmath>
 #include "/home/paul/Projekte/htlib/src/cpp_interface/htl_m_cpp_interface.hpp"
+//#include <dergeraet>
 
-void test_function(int** ind, double** val)
+
+// Interface mit dergeraet schreiben.
+// (int**) ueberall, sonst funktioniert die Uebergabe zu Fortran nicht...
+
+void test_function(int* ind, double &val)
 {
-  val[0][0] = 1.0/(ind[0][0]);
+	size_t nx = 100;
+	size_t ny = 100;
+
+    constexpr double alpha = 0.01;
+    constexpr double k     = 0.5;
+    double x = (ind[0]-1)*4*M_PI/nx;
+    double u = -5 + (ind[1]-1)*10/ny;
+
+	val = 0.39894228040143267793994 * ( 1. + alpha*cos(k*x) ) * exp( -u*u/2. ) * u*u;
 }
 
-template <size_t d>
-void templated_test_function(int** ind, double** val)
+
+
+//template <size_t d>
+void test_function_2(int** ind, double& val)
 {
-	val[0][0] = 1.0/(ind[0][d]);
+	// ind: Indizes des Gitters
+	// val: Return value
+
+	val = 1.0/((ind[0][0]-1)+(ind[0][1]-1));
 }
 
 
 
 int main(int argc, char **argv)
 {
-  void* htensor;
-  auto htensorPtr= &htensor;
-  int32_t d = 2;
-  auto dPtr = &d;
-  int32_t n[2]{10,10};
-  int32_t* nPtr1 = &n[0];
-  double* vec[10*10];
-  auto vecPtr = &vec[0];
-  int32_t size = 10*10;
-  auto sizePtr = &size;
+	  void* htensor;
+	  auto htensorPtr= &htensor;
+	  int32_t d = 2;
+	  auto dPtr = &d;
+	  int32_t n[2]{100,100};
+	  int32_t* nPtr1 = &n[0];
+	  double* vec[100*100];
+	  auto vecPtr = &vec[0];
+	  int32_t size = 100*100;
+	  auto sizePtr = &size;
+	  auto fctPtr = &test_function_2;
 
-  auto lambda_test_fct = [](int** ind, double ** val){ // Hier darf man nix in die [] Klammern schreiben. Referenzen auf ausserhalb sind bei
-	  	  	  	  	  	  	  	  	  	  	  	  	   // Fct-pointern auf lambdas nicht erlaubt.
-	val[0][0] = 1.0/(ind[0][0]);
-  };
+	  bool is_rand = true;
 
-  //void (*fctPtr)(int**,double**) = &test_function;
-  //void (*fctPtr)(int**,double**) = lambda_test_fct;
-  void (*fctPtr)(int**,double**) = templated_test_function<2>;
+	  void* opts;
+	  auto optsPtr = &opts;
 
-  bool is_rand = true;
+	  double tol = 1e-8;
+	  int32_t tcase = 1;
 
-  void* opts;
-  auto optsPtr = &opts;
+	  int32_t cross_no_loops = 3;
+	  int32_t nNodes = 3;
+	  int32_t rank = 20;
+	  int32_t rank_rand_row = 10;
+	  int32_t rank_rand_col = 10;
 
-  chtl_s_htensor_init_balanced(htensorPtr, dPtr, &nPtr1);
-  //chtl_s_cross(fctPtr, htensorPtr, optsPtr, &is_rand);
-  chtl_s_htensor_vectorize(htensorPtr, vecPtr, sizePtr);
-  
-  std::cout << "I'm done." << std::endl;
+	  chtl_s_init_truncation_option(optsPtr, &tcase, &tol, &cross_no_loops, &nNodes, &rank, &rank_rand_row, &rank_rand_col);
+	  chtl_s_htensor_init_balanced(htensorPtr, dPtr, nPtr1);
+	  chtl_s_cross(fctPtr, htensorPtr, optsPtr, &is_rand);
+	  chtl_s_htensor_vectorize(htensorPtr, vecPtr, sizePtr);
+	  // vecptr rekonstruiert alle werte auf dem gitter und kann genutzt werden, um zu testen, ob
+	  // die approx gut ist.
+
+	  std::cout << "I'm done." << std::endl;
   
 }
