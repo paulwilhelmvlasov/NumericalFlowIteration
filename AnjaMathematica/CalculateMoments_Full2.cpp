27ce2bbc986a62d6cc6804ea3b8d2c1bdcc7357e
Anja Matena <anja.matena@rwth-aachen.de>
2024-03-20 11:51:07 +0100

    Only one eval_f call + Update tests

--- AnjaMathematica/CalculateMoments_Full2.cpp
+++ AnjaMathematica/CalculateMoments_Full2.cpp
@@ -5,6 +5,7 @@
 #include <cstddef>
 #include <cmath>
 #include <algorithm>
+#include <vector>
 
 #include <dergeraet/config.hpp>
 
@@ -34,6 +35,8 @@ void eval_moments( size_t n, size_t l, const real *coeffs, const config_t<real> 
 	real j_z = 0;
 	real std_dev = 0;
 
+	std::vector<std::vector<std::vector<real>>> f(conf.Nu, std::vector<std::vector<real>>(conf.Nv, std::vector<real>(conf.Nw)));
+
 	for ( size_t kk = 0; kk < conf.Nw; ++kk )
 	for ( size_t jj = 0; jj < conf.Nv; ++jj )
 	for ( size_t ii = 0; ii < conf.Nu; ++ii )
@@ -41,12 +44,14 @@ void eval_moments( size_t n, size_t l, const real *coeffs, const config_t<real> 
 		real u = u_min + ii*du;
 		real v = v_min + jj*dv;
 		real w = w_min + kk*dw;
-		real f = eval_f<real,order>( n, x, y, z, u, v, w, coeffs, conf );
+		real func = eval_f<real,order>( n, x, y, z, u, v, w, coeffs, conf );
 
-		rho += f;
-		j_x += f*u;
-		j_y += f*v;
-		j_z += f*w;
+		rho += func;
+		j_x += func * u;
+		j_y += func * v;
+		j_z += func * w;
+
+		f[ii][jj][kk] = func;
 	}
 
 	// Calculate vmean
@@ -59,17 +64,12 @@ void eval_moments( size_t n, size_t l, const real *coeffs, const config_t<real> 
 	for ( size_t jj = 0; jj < conf.Nv; ++jj )
 	for ( size_t ii = 0; ii < conf.Nu; ++ii )
 	{
-		real u = u_min + ii * du;
-		real v = v_min + jj * dv;
-		real w = w_min + kk * dw;
-		real f = eval_f<real,order>( n, x, y, z, u, v, w, coeffs, conf );
-
 		// vmean = 0
-		real unew = u - j_x;
-		real vnew = v - j_y;
-		real wnew = w - j_z;
+		real unew = u_min + ii*du - j_x;
+		real vnew = v_min + jj*dv - j_y;
+		real wnew = w_min + kk*dw - j_z;
 
-		std_dev += f * 1/3 * (pow(unew, 2) + pow(vnew, 2) + pow(wnew, 2));
+		std_dev += f[ii][jj][kk] * 1/3 * (pow(unew, 2) + pow(vnew, 2) + pow(wnew, 2));
 	}
 
 	std_dev = sqrt(std_dev / rho); // Standard deviation
@@ -79,53 +79,52 @@ void eval_moments( size_t n, size_t l, const real *coeffs, const config_t<real> 
 	for ( size_t jj = 0; jj < conf.Nv; ++jj )
 	for ( size_t ii = 0; ii < conf.Nu; ++ii )
 	{
-		real u = u_min + ii*du;
-		real v = v_min + jj*dv;
-		real w = w_min + kk*dw;
-		real f = eval_f<real,order>( n, x, y, z, u, v, w, coeffs, conf );
-
 		// std_dev = 1
-		real unew = (u - j_x)/std_dev;
-		real vnew = (v - j_y)/std_dev;
-		real wnew = (w - j_z)/std_dev;
+		real unew = (u_min + ii*du - j_x)/std_dev;
+		real vnew = (v_min + jj*dv - j_y)/std_dev;
+		real wnew = (w_min + kk*dw - j_z)/std_dev;
 
-		if(rho != 0) {
+		if(rho < pow(10,-6)) {
+			for (int i = 0; i < 35; i++) {
+				moments[i] = 0;
+			}
+		} else {
 
-			moments[0] += f/rho * 1; 
-			moments[1] += f/rho * sqrt(0.6666666666666666)*(1.5 - pow(unew,2)/2. - pow(vnew,2)/2. - pow(wnew,2)/2.); 
-			moments[2] += f/rho * sqrt(0.13333333333333333)*(3.75 - (5*pow(unew,2))/2. + pow(unew,4)/4. - (5*pow(vnew,2))/2. + (pow(unew,2)*pow(vnew,2))/2. + pow(vnew,4)/4. - (5*pow(wnew,2))/2. + (pow(unew,2)*pow(wnew,2))/2. + (pow(vnew,2)*pow(wnew,2))/2. + pow(wnew,4)/4.); 
-			moments[3] += f/rho * vnew; 
-			moments[4] += f/rho * wnew; 
-			moments[5] += f/rho * -unew; 
-			moments[6] += f/rho * sqrt(0.4)*((5*vnew)/2. - (pow(unew,2)*vnew)/2. - pow(vnew,3)/2. - (vnew*pow(wnew,2))/2.); 
-			moments[7] += f/rho * sqrt(0.4)*((5*wnew)/2. - (pow(unew,2)*wnew)/2. - (pow(vnew,2)*wnew)/2. - pow(wnew,3)/2.); 
-			moments[8] += f/rho * sqrt(0.4)*((-5*unew)/2. + pow(unew,3)/2. + (unew*pow(vnew,2))/2. + (unew*pow(wnew,2))/2.); 
-			moments[9] += f/rho * unew*vnew; 
-			moments[10] += f/rho * vnew*wnew; 
-			moments[11] += f/rho * (-pow(unew,2) - pow(vnew,2) + 2*pow(wnew,2))/(2.*sqrt(3)); 
-			moments[12] += f/rho * -(unew*wnew); 
-			moments[13] += f/rho * (pow(unew,2) - pow(vnew,2))/2.; 
-			moments[14] += f/rho * sqrt(0.2857142857142857)*((7*unew*vnew)/2. - (pow(unew,3)*vnew)/2. - (unew*pow(vnew,3))/2. - (unew*vnew*pow(wnew,2))/2.); 
-			moments[15] += f/rho * sqrt(0.2857142857142857)*((7*vnew*wnew)/2. - (pow(unew,2)*vnew*wnew)/2. - (pow(vnew,3)*wnew)/2. - (vnew*pow(wnew,3))/2.); 
-			moments[16] += f/rho * ((-7*pow(unew,2))/2. + pow(unew,4)/2. - (7*pow(vnew,2))/2. + pow(unew,2)*pow(vnew,2) + pow(vnew,4)/2. + 7*pow(wnew,2) - (pow(unew,2)*pow(wnew,2))/2. - (pow(vnew,2)*pow(wnew,2))/2. - pow(wnew,4))/sqrt(42); 
-			moments[17] += f/rho * sqrt(0.2857142857142857)*((-7*unew*wnew)/2. + (pow(unew,3)*wnew)/2. + (unew*pow(vnew,2)*wnew)/2. + (unew*pow(wnew,3))/2.); 
-			moments[18] += f/rho * ((7*pow(unew,2))/2. - pow(unew,4)/2. - (7*pow(vnew,2))/2. + pow(vnew,4)/2. - (pow(unew,2)*pow(wnew,2))/2. + (pow(vnew,2)*pow(wnew,2))/2.)/sqrt(14); 
-			moments[19] += f/rho * (3*pow(unew,2)*vnew - pow(vnew,3))/(2.*sqrt(6)); 
-			moments[20] += f/rho * unew*vnew*wnew; 
-			moments[21] += f/rho * (-(pow(unew,2)*vnew) - pow(vnew,3) + 4*vnew*pow(wnew,2))/(2.*sqrt(10)); 
-			moments[22] += f/rho * (-3*pow(unew,2)*wnew - 3*pow(vnew,2)*wnew + 2*pow(wnew,3))/(2.*sqrt(15)); 
-			moments[23] += f/rho * (pow(unew,3) + unew*pow(vnew,2) - 4*unew*pow(wnew,2))/(2.*sqrt(10)); 
-			moments[24] += f/rho * (pow(unew,2)*wnew - pow(vnew,2)*wnew)/2.; 
-			moments[25] += f/rho * (-pow(unew,3) + 3*unew*pow(vnew,2))/(2.*sqrt(6)); 
-			moments[26] += f/rho * (pow(unew,3)*vnew - unew*pow(vnew,3))/(2.*sqrt(3)); 
-			moments[27] += f/rho * (3*pow(unew,2)*vnew*wnew - pow(vnew,3)*wnew)/(2.*sqrt(6)); 
-			moments[28] += f/rho * (-(pow(unew,3)*vnew) - unew*pow(vnew,3) + 6*unew*vnew*pow(wnew,2))/(2.*sqrt(21)); 
-			moments[29] += f/rho * (-3*pow(unew,2)*vnew*wnew - 3*pow(vnew,3)*wnew + 4*vnew*pow(wnew,3))/(2.*sqrt(42)); 
-			moments[30] += f/rho * (3*pow(unew,4) + 6*pow(unew,2)*pow(vnew,2) + 3*pow(vnew,4) - 24*pow(unew,2)*pow(wnew,2) - 24*pow(vnew,2)*pow(wnew,2) + 8*pow(wnew,4))/(8.*sqrt(105)); 
-			moments[31] += f/rho * (3*pow(unew,3)*wnew + 3*unew*pow(vnew,2)*wnew - 4*unew*pow(wnew,3))/(2.*sqrt(42)); 
-			moments[32] += f/rho * (-pow(unew,4) + pow(vnew,4) + 6*pow(unew,2)*pow(wnew,2) - 6*pow(vnew,2)*pow(wnew,2))/(4.*sqrt(21)); 
-			moments[33] += f/rho * (-(pow(unew,3)*wnew) + 3*unew*pow(vnew,2)*wnew)/(2.*sqrt(6)); 
-			moments[34] += f/rho * (pow(unew,4) - 6*pow(unew,2)*pow(vnew,2) + pow(vnew,4))/(8.*sqrt(3)); 
+			moments[0] += f[ii][jj][kk]/rho * 1.; 
+			moments[1] += f[ii][jj][kk]/rho * 0.816496580927726*(1.5 - 0.5*pow(unew,2) - 0.5*pow(vnew,2) - 0.5*pow(wnew,2)); 
+			moments[2] += f[ii][jj][kk]/rho * 0.3651483716701107*(3.75 - 2.5*pow(unew,2) + 0.25*pow(unew,4) - 2.5*pow(vnew,2) + 0.5*pow(unew,2)*pow(vnew,2) + 0.25*pow(vnew,4) - 2.5*pow(wnew,2) + 0.5*pow(unew,2)*pow(wnew,2) + 0.5*pow(vnew,2)*pow(wnew,2) + 0.25*pow(wnew,4)); 
+			moments[3] += f[ii][jj][kk]/rho * vnew; 
+			moments[4] += f[ii][jj][kk]/rho * wnew; 
+			moments[5] += f[ii][jj][kk]/rho * -1.*unew; 
+			moments[6] += f[ii][jj][kk]/rho * 0.6324555320336759*(2.5*vnew - 0.5*pow(unew,2)*vnew - 0.5*pow(vnew,3) - 0.5*vnew*pow(wnew,2)); 
+			moments[7] += f[ii][jj][kk]/rho * 0.6324555320336759*(2.5*wnew - 0.5*pow(unew,2)*wnew - 0.5*pow(vnew,2)*wnew - 0.5*pow(wnew,3)); 
+			moments[8] += f[ii][jj][kk]/rho * 0.6324555320336759*(-2.5*unew + 0.5*pow(unew,3) + 0.5*unew*pow(vnew,2) + 0.5*unew*pow(wnew,2)); 
+			moments[9] += f[ii][jj][kk]/rho * unew*vnew; 
+			moments[10] += f[ii][jj][kk]/rho * vnew*wnew; 
+			moments[11] += f[ii][jj][kk]/rho * 0.2886751345948129*(-1.*pow(unew,2) - 1.*pow(vnew,2) + 2.*pow(wnew,2)); 
+			moments[12] += f[ii][jj][kk]/rho * -1.*unew*wnew; 
+			moments[13] += f[ii][jj][kk]/rho * 0.5*(pow(unew,2) - 1.*pow(vnew,2)); 
+			moments[14] += f[ii][jj][kk]/rho * 0.5345224838248488*(3.5*unew*vnew - 0.5*pow(unew,3)*vnew - 0.5*unew*pow(vnew,3) - 0.5*unew*vnew*pow(wnew,2)); 
+			moments[15] += f[ii][jj][kk]/rho * 0.5345224838248488*(3.5*vnew*wnew - 0.5*pow(unew,2)*vnew*wnew - 0.5*pow(vnew,3)*wnew - 0.5*vnew*pow(wnew,3)); 
+			moments[16] += f[ii][jj][kk]/rho * 0.1543033499620919*(-3.5*pow(unew,2) + 0.5*pow(unew,4) - 3.5*pow(vnew,2) + pow(unew,2)*pow(vnew,2) + 0.5*pow(vnew,4) + 7.*pow(wnew,2) - 0.5*pow(unew,2)*pow(wnew,2) - 0.5*pow(vnew,2)*pow(wnew,2) - 1.*pow(wnew,4)); 
+			moments[17] += f[ii][jj][kk]/rho * 0.5345224838248488*(-3.5*unew*wnew + 0.5*pow(unew,3)*wnew + 0.5*unew*pow(vnew,2)*wnew + 0.5*unew*pow(wnew,3)); 
+			moments[18] += f[ii][jj][kk]/rho * 0.2672612419124244*(3.5*pow(unew,2) - 0.5*pow(unew,4) - 3.5*pow(vnew,2) + 0.5*pow(vnew,4) - 0.5*pow(unew,2)*pow(wnew,2) + 0.5*pow(vnew,2)*pow(wnew,2)); 
+			moments[19] += f[ii][jj][kk]/rho * 0.20412414523193154*(3.*pow(unew,2)*vnew - 1.*pow(vnew,3)); 
+			moments[20] += f[ii][jj][kk]/rho * unew*vnew*wnew; 
+			moments[21] += f[ii][jj][kk]/rho * 0.15811388300841897*(-1.*pow(unew,2)*vnew - 1.*pow(vnew,3) + 4.*vnew*pow(wnew,2)); 
+			moments[22] += f[ii][jj][kk]/rho * 0.12909944487358055*(-3.*pow(unew,2)*wnew - 3.*pow(vnew,2)*wnew + 2.*pow(wnew,3)); 
+			moments[23] += f[ii][jj][kk]/rho * 0.15811388300841897*(pow(unew,3) + unew*pow(vnew,2) - 4.*unew*pow(wnew,2)); 
+			moments[24] += f[ii][jj][kk]/rho * 0.5*(pow(unew,2)*wnew - 1.*pow(vnew,2)*wnew); 
+			moments[25] += f[ii][jj][kk]/rho * 0.20412414523193154*(-1.*pow(unew,3) + 3.*unew*pow(vnew,2)); 
+			moments[26] += f[ii][jj][kk]/rho * 0.2886751345948129*(pow(unew,3)*vnew - 1.*unew*pow(vnew,3)); 
+			moments[27] += f[ii][jj][kk]/rho * 0.20412414523193154*(3.*pow(unew,2)*vnew*wnew - 1.*pow(vnew,3)*wnew); 
+			moments[28] += f[ii][jj][kk]/rho * 0.1091089451179962*(-1.*pow(unew,3)*vnew - 1.*unew*pow(vnew,3) + 6.*unew*vnew*pow(wnew,2)); 
+			moments[29] += f[ii][jj][kk]/rho * 0.07715167498104596*(-3.*pow(unew,2)*vnew*wnew - 3.*pow(vnew,3)*wnew + 4.*vnew*pow(wnew,3)); 
+			moments[30] += f[ii][jj][kk]/rho * 0.012198750911856666*(3.*pow(unew,4) + 6.*pow(unew,2)*pow(vnew,2) + 3.*pow(vnew,4) - 24.*pow(unew,2)*pow(wnew,2) - 24.*pow(vnew,2)*pow(wnew,2) + 8.*pow(wnew,4)); 
+			moments[31] += f[ii][jj][kk]/rho * 0.07715167498104596*(3.*pow(unew,3)*wnew + 3.*unew*pow(vnew,2)*wnew - 4.*unew*pow(wnew,3)); 
+			moments[32] += f[ii][jj][kk]/rho * 0.0545544725589981*(-1.*pow(unew,4) + pow(vnew,4) + 6.*pow(unew,2)*pow(wnew,2) - 6.*pow(vnew,2)*pow(wnew,2)); 
+			moments[33] += f[ii][jj][kk]/rho * 0.20412414523193154*(-1.*pow(unew,3)*wnew + 3.*unew*pow(vnew,2)*wnew); 
+			moments[34] += f[ii][jj][kk]/rho * 0.07216878364870323*(pow(unew,4) - 6.*pow(unew,2)*pow(vnew,2) + pow(vnew,4)); 
 		}
 	}
 	moments[35] = rho; 
--- AnjaMathematica/TestCalculateMoments.cpp
+++ AnjaMathematica/TestCalculateMoments.cpp
@@ -1,9 +1,22 @@
-//#include "CalculateMoments_444444444.cpp" // 4444444444
-//#include "CalculateMoments_3333333.cpp" // 3333333
-#include "CalculateMoments_Full10.cpp" // Full10
 #include <vector>
 #include <iostream>
 #include <cmath>
+
+// for testing: remove #include <dergeraet/config.hpp> from CalculateMoments_<<theory>>.cpp and add the following statement
+// #include "test_def.hpp"
+
+// terminal commands:
+/*
+g++ -c -std=c++20 CalculateMoments_<<theory>>.cpp
+g++ -c -std=c++20 TestCalculateMoments.cpp
+g++ TestCalculateMoments.o CalculateMoments_<<theory>>.o -o Test
+./Test
+*/
+
+// Select theory
+#include "CalculateMoments_3333333.cpp"
+// #include "CalculateMoments_444444444.cpp"
+// #include "CalculateMoments_Full10.cpp"
 
 double runde(double value, int precision)
 {
@@ -11,42 +24,127 @@ double runde(double value, int precision)
   return (double)((int)(value * multiplier + 0.5f))/multiplier;
 }
 
-int main () {
-    std::vector<double> coeffs; // aktuell egal
-    std::vector<double> moments;
-    //moments.resize(410); // 4444444444
-    //moments.resize(201); // 3333333
-    moments.resize(1776); // Full10
+void gauss3333333 () {
+  std::vector<double> coeffs; // aktuell egal
+  std::vector<double> moments;
+  moments.resize(201);
 
-    config_t<double> conf;
-    conf.Nx = conf.Ny = conf.Nz = 1;  // Number of grid points in physical space.
-    conf.Nu = conf.Nv = conf.Nw = 20;  // Number of quadrature points in velocity space.
-    //conf.Nt = 50;  // Number of time-steps.
-    //conf.dt = 1;  // Time-step size.
+  config_t<double> conf;
+  conf.Nx = conf.Ny = conf.Nz = 1;  // Number of grid points in physical space.
+  conf.Nu = conf.Nv = conf.Nw = 20;  // Number of quadrature points in velocity space.
+  //conf.Nt = 50;  // Number of time-steps.
+  //conf.dt = 1;  // Time-step size.
 
-    // Dimensions of physical domain.
-    conf.x_min = conf.y_min = conf.z_min = 0; // aktuell egal
-    conf.x_max = conf.y_max = conf.z_max = 1; // aktuell egal
+  // Dimensions of physical domain.
+  conf.x_min = conf.y_min = conf.z_min = 0; // aktuell egal
+  conf.x_max = conf.y_max = conf.z_max = 1; // aktuell egal
 
-    // Integration limits for velocity space.
-    conf.u_min = conf.v_min = conf.w_min = -10; // ausprobieren
-    conf.u_max = conf.v_max = conf.w_max = 10;
+  // Integration limits for velocity space.
+  conf.u_min = conf.v_min = conf.w_min = -10; // ausprobieren
+  conf.u_max = conf.v_max = conf.w_max = 10;
 
-    // Grid-sizes and their reciprocals.
-    conf.dx = conf.dy = conf.dz = 1; // aktuell egal
-    //conf.dx_inv = conf.dy_inv = conf.dz_inv = 1;
-    //conf.Lx = conf.Ly = conf.Lz = 1;
-    //conf.Lx_inv = conf.Ly_inv = conf.Lz_inv = 1;
-    conf.du = conf.dv = conf.dw = 0.5; // ausprobieren
+  // Grid-sizes and their reciprocals.
+  conf.dx = conf.dy = conf.dz = 1; // aktuell egal
+  //conf.dx_inv = conf.dy_inv = conf.dz_inv = 1;
+  //conf.Lx = conf.Ly = conf.Lz = 1;
+  //conf.Lx_inv = conf.Ly_inv = conf.Lz_inv = 1;
+  conf.du = conf.dv = conf.dw = 0.5; // ausprobieren
 
-    size_t n = 1; // aktuell egal
-    size_t l = 1; // aktuell egal
+  size_t n = 1; // aktuell egal
+  size_t l = 1; // aktuell egal
 
-    constexpr size_t order = 1; // aktuell egal
+  constexpr size_t order = 1; // aktuell egal
 
-    eval_moments<double, order>(n, l, &coeffs[0], conf, &moments[0] );
+  eval_moments<double, order>(n, l, &coeffs[0], conf, &moments[0] );
 
-    for(int i = 0; i < moments.size(); i++) {
+  for(int i = 0; i < moments.size(); i++) {
         std::cout << "moments[" << i << "] = " << runde(moments[i],6)  << std::endl;
-    }
+  }
+}
+
+void gauss444444444 () {
+  std::vector<double> coeffs; // aktuell egal
+  std::vector<double> moments;
+  moments.resize(410);
+
+  config_t<double> conf;
+  conf.Nx = conf.Ny = conf.Nz = 1;  // Number of grid points in physical space.
+  conf.Nu = conf.Nv = conf.Nw = 20;  // Number of quadrature points in velocity space.
+  //conf.Nt = 50;  // Number of time-steps.
+  //conf.dt = 1;  // Time-step size.
+
+  // Dimensions of physical domain.
+  conf.x_min = conf.y_min = conf.z_min = 0; // aktuell egal
+  conf.x_max = conf.y_max = conf.z_max = 1; // aktuell egal
+
+  // Integration limits for velocity space.
+  conf.u_min = conf.v_min = conf.w_min = -10; // ausprobieren
+  conf.u_max = conf.v_max = conf.w_max = 10;
+
+  // Grid-sizes and their reciprocals.
+  conf.dx = conf.dy = conf.dz = 1; // aktuell egal
+  //conf.dx_inv = conf.dy_inv = conf.dz_inv = 1;
+  //conf.Lx = conf.Ly = conf.Lz = 1;
+  //conf.Lx_inv = conf.Ly_inv = conf.Lz_inv = 1;
+  conf.du = conf.dv = conf.dw = 0.5; // ausprobieren
+
+  size_t n = 1; // aktuell egal
+  size_t l = 1; // aktuell egal
+
+  constexpr size_t order = 1; // aktuell egal
+
+  eval_moments<double, order>(n, l, &coeffs[0], conf, &moments[0] );
+
+  for(int i = 0; i < moments.size(); i++) {
+        std::cout << "moments[" << i << "] = " << runde(moments[i],6)  << std::endl;
+  }
+}
+
+void Full10 () {
+  std::vector<double> coeffs; // aktuell egal
+  std::vector<double> moments;
+  moments.resize(1776);
+
+  config_t<double> conf;
+  conf.Nx = conf.Ny = conf.Nz = 1;  // Number of grid points in physical space.
+  conf.Nu = conf.Nv = conf.Nw = 20;  // Number of quadrature points in velocity space.
+  //conf.Nt = 50;  // Number of time-steps.
+  //conf.dt = 1;  // Time-step size.
+
+  // Dimensions of physical domain.
+  conf.x_min = conf.y_min = conf.z_min = 0; // aktuell egal
+  conf.x_max = conf.y_max = conf.z_max = 1; // aktuell egal
+
+  // Integration limits for velocity space.
+  conf.u_min = conf.v_min = conf.w_min = -10; // ausprobieren
+  conf.u_max = conf.v_max = conf.w_max = 10;
+
+  // Grid-sizes and their reciprocals.
+  conf.dx = conf.dy = conf.dz = 1; // aktuell egal
+  //conf.dx_inv = conf.dy_inv = conf.dz_inv = 1;
+  //conf.Lx = conf.Ly = conf.Lz = 1;
+  //conf.Lx_inv = conf.Ly_inv = conf.Lz_inv = 1;
+  conf.du = conf.dv = conf.dw = 0.5; // ausprobieren
+
+  size_t n = 1; // aktuell egal
+  size_t l = 1; // aktuell egal
+
+  constexpr size_t order = 1; // aktuell egal
+
+  eval_moments<double, order>(n, l, &coeffs[0], conf, &moments[0] );
+
+  for(int i = 0; i < moments.size(); i++) {
+        std::cout << "moments[" << i << "] = " << runde(moments[i],6)  << std::endl;
+  }
+}
+
+int main () {
+
+  gauss3333333(); 
+
+  // gauss444444444();
+  
+  // Full10();
+
+  return 0;
 }
