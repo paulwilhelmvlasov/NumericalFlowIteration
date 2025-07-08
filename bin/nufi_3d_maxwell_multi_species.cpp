a995434c9ed7c4d6caae256db25471f563c564d7
Paul Wilhelm <paul.wilhelm@kuleuven.be>
2025-07-08 13:33:50 +0200

    reduced output frequency

--- bin/nufi_3d_maxwell_multi_species.cpp
+++ bin/nufi_3d_maxwell_multi_species.cpp
@@ -548,7 +548,7 @@ void do_stats(size_t nt, size_t nx_plot, std::ofstream& stat_file,
         current_time = nt * conf.dt;
     }
 
-    if(/* nt % (5*steps_per_1) == 0 */ true){
+    if(nt % (5*steps_per_1) == 0){
         std::ofstream Ex_str("Ex_" + std::to_string(current_time) + ".txt");
         std::ofstream Ey_str("Ey_" + std::to_string(current_time) + ".txt");
         std::ofstream Ez_str("Ez_" + std::to_string(current_time) + ".txt");
